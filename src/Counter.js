13646388c7e4985e7dfda34dc9e23dfbf86c9c2a
DohnalMichal <michal.dohnal7@gmail.com>
2021-03-23 21:10:16 +0100

    Counter works

--- src/Counter.js
+++ src/Counter.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Counter extends Component {
-  render() {
-    return (
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
+          <button onClick={() => setCount(count - count)}> Reset</button>
         </section>
-
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default Counter;
+export default Counter
